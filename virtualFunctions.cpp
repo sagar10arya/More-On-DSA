2b9275b09d8b64caeee2d089cbd2bf205f4fcfc7
sagar10arya <aryasagar125@gmail.com>
2024-06-18 17:27:44 +0530

    Having Fun with DSA

---
+++ virtualFunctions.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class BaseClass{
+    public:
+    int var_base=1;
+    virtual void display(){
+        cout<<"Displaying Base Class variable var_base "<<var_base<<endl;
+    }
+};
+
+class DerivedClass : public BaseClass{
+    public:
+    int var_derived = 2;
+    void display(){
+        cout<<"2 Displaying Base Class variable var_base "<<var_base<<endl;
+        cout<<"2 Displaying Derived Class variable var_derived "<<var_derived<<endl;
+    }
+};
+
+int main(){
+    BaseClass *base_class_pointer;
+    BaseClass obj_base;
+    DerivedClass obj_derived;
+
+    base_class_pointer = &obj_derived;
+    base_class_pointer->display();
+
+/*
+The main thing to note here is that if we don’t use the “virtual” keyword with the “display” function of the base class then beside of the point that we have pointed our base call pointer to derived class object still the compiler would have called the “display” function of the base class because this is its default behavior as we have seen in the previous tutorial.
+
+But we have used the “virtual” keyword with the “display” function of the base class to make is virtual function so when the display function is called by using the base class pointer the display function of the derived class will run because the base class pointer is pointing to the derived class object.
+*/
+
+    return 0;
+}
